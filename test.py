21f62c0ad24f5727d52c95ebeb6291ad6ff1dd82
neil4droid <cterse@gmail.com>
2018-03-20 21:24:31 +0530

    created test plot

--- test.py
+++ test.py
@@ -3,14 +3,24 @@ Created on 18-Mar-2018
 
 @author: DELL
 '''
-from _datetime import datetime
 
-date = "2008\n"
-print(date)
-date = date.rstrip()
-print(date)
-print(date)
+import matplotlib.pyplot as plt
+import read_files
 
-dt_onj = datetime.today().year
-print(type(dt_onj))
-print(dt_onj)
+movie_list = read_files.get_movie_list()
+movies_per_year_dict = {}
+
+for x in movie_list:
+    movies_per_year_dict[int(x.get_release_year())] = 0
+
+for x in movie_list:
+    y = movies_per_year_dict.get(int(x.get_release_year()))
+    y += 1
+    movies_per_year_dict[int(x.get_release_year())] = y
+
+print(movies_per_year_dict)
+
+x = movies_per_year_dict.keys()
+y = movies_per_year_dict.values()
+plt.plot(x, y)
+plt.show()
